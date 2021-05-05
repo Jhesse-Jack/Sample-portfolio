1d6e363531a535b71ddd79d12e24d00f65f29c7b
Jhesse-Jack <armahjames9@gmail.com>
2021-05-05 21:49:13 +0000

    Will start working on about me page soon

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,60 +1,105 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import "../App.scss";
-import "../bootstrap.min.css";
-import Wavy from "../images/bye.svg"
-import Content from './landingpagecontent';
+// import React from 'react';
+// import { Link } from 'gatsby';
+// import "../App.scss";
+// import "../bootstrap.min.css";
+// import Content from './landingpagecontent';
+// import './style.css';
 
-const StartPage = () => {
+// const StartPage = () => {
+//     return (
+//         <div className="container-fluid landingpage">
+//             <header id="header" style={styles.header}>
+//                 <nav className="right">
+//                     <Link to="/aboutme" className="button alt">
+//                         About Me
+//                     </Link>
+//                     {/* <Link to="lets-talk-reactjs"className="button alt">
+//                         Blog
+//                     </Link> */}
+//                     <a href="https://lets-talk-reactjs.netlify.app" className="button alt">
+//                         Blog
+//                     </a>
+//                     <Link to="/projects" className="button alt">
+//                         Projects
+//                     </Link>
+//                 </nav>
+//             </header>
+//             <Content />
+//         </div>
+//     )
+// }
+
+// const styles = {
+//     header: {
+//         margin: '0%',
+//         padding: '0%',
+//         border: '0',
+//         fontSize: '100%',
+//         font: 'inherit',
+//         verticalAlign: 'baseline',
+//         display: 'block',
+//     },
+
+// };
+
+// export default StartPage;
+
+import React from 'react';
+import '../bootstrap.min.css';
+import Content from './landingpagecontent';
+import { Link } from 'gatsby';
+//import '../App.scss';
+import './style.scss';
+
+const startPage = () => {
     return (
-        <div className="container-fluid">
-            <header id="header" style={styles.header}>
-                <nav className="right">
-                    <Link to="/aboutme" className="button alt">
-                        About Me
-                    </Link>
-                    {/* <Link to="lets-talk-reactjs"className="button alt">
-                        Blog
-                    </Link> */}
-                    <a href="https://lets-talk-reactjs.netlify.app" className="button alt">
-                        Blog
-                    </a>
-                    <Link to="/projects" className="button alt">
-                        Projects
-                    </Link>
-                </nav>
+        //Header
+        <div className="landingpage">
+            <header className="header">
+                <div className="container row">
+                    <nav className="navbar navbar expand-lg align-content-center px-0">
+                        <button 
+                            className="navbar-toggler" 
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#menu"
+                            aria-controls="menu"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <i className="fas-fa bars"></i>
+                        </button>
+                        <div className="navbar-collapse" id="menu">
+                            <ul className="navbar-nav ml-lg-auto row">
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link active">
+                                        Home
+                                        <span className="sr-only"> (current) </span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/aboutme" className="nav-link ips">
+                                        About Me
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link ips" href="https://lets-talk-reactjs.netlify.app">
+                                        Blog
+                                    </a>
+                                </li>
+                                <li className="nav-item nav-item-divider d-none d-lg-block"></li>
+                                <li className="nav-item">
+                                    <Link to="/features" className="nav-link ips">
+                                        Projects
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </header>
             <Content />
-            {/* <section>
-                <h2> 
-                    Hello there, 
-                    <img src={Wavy} alt="wavy-hand" />
-                    <br />
-                    <span> 
-                        I'm 
-                        <span>
-                            James 
-                        </span>
-                    </span>
-                    <br />
-                    A front-end developer
-                </h2>
-            </section> */}
         </div>
     )
 }
 
-const styles = {
-    header: {
-        margin: '0%',
-        padding: '0%',
-        border: '0',
-        fontSize: '100%',
-        font: 'inherit',
-        verticalAlign: 'baseline',
-        display: 'block',
-    },
-
-};
-
-export default StartPage;
+export default startPage;
--- src/components/landingpagecontent.js
+++ src/components/landingpagecontent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../bootstrap.min.css';
-import './style.css';
+import './style.scss';
 import Wavy from '../images/bye.svg';
 import HeroImage from '../images/undraw_coding_6mjf.svg';
 
 const Hero = () => {
 	return (
-		<div className="jumbotron jumbotron-fluid bg-transparent hero section m-0">
+		<div className="jumbotron jumbotron-fluid bg-transparent hero section m-0 landingpage">
 			<div className="container">
 				<div className="row align-items-end align-items-xl-center">
 					<div className="col-12 col-lg-5">
@@ -20,11 +20,15 @@ const Hero = () => {
 								<span>
 									<b>
 									I'm
-									<span> James</span> </b>
+									<span className="name"> James</span> </b>
 								&nbsp;
 								</span>
 								<br />
-								<b> A &lt; front-end developer /&gt; </b>
+								<b> A 
+									<span>
+										&lt; front-end developer /&gt; 
+									</span>
+								</b>
 							</p>
 							&nbsp;
 							<div className="space-md" />
